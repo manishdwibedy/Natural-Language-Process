34663eee0e3fd506c6432e659b584b468485da8d
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-04-10 19:20:24 -0700

    Creating method to get the path of the data directory and read a file in the data directory.

--- Homework 3/util.py
+++ Homework 3/util.py
@@ -1,4 +1,5 @@
 import os
+import constant
 
 def changeToPresentDirectory():
     '''
@@ -15,6 +16,34 @@ def getCurrentPath():
     '''
     return os.getcwd()
 
+def getDataDirectory():
+    '''
+    Getting the data directory
+    :return: Returns the data directory's absolute path
+    '''
+    return os.path.join(getCurrentPath(), constant.DATA_DIR)
+
+def readData(filename):
+    '''
+    Readging the data file, specified by the filename
+    :param filename: The filename to be read
+    :return: A list containing every line in the file
+    '''
+
+    # File Content list
+    fileContents = []
+
+    # Generating the file's absolute path
+    path = os.path.join(getDataDirectory(), filename)
+
+    with open(path) as file:
+        for line in file:
+            fileContents.append(line)
+
+    return fileContents
+
+
 if __name__ == '__main__':
-    changeToPresentDirectory()
-    print getCurrentPath()
+    # changeToPresentDirectory()
+    # print getCurrentPath()
+    print readData('catalan_corpus_dev_tagged.txt')
