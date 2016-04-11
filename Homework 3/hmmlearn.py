c5a3467502a79604e1b8f4623412811cce30aab6
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-04-10 20:12:54 -0700

    Getting the starting tag count to help compute the starting probabilities.

--- Homework 3/hmmlearn.py
+++ Homework 3/hmmlearn.py
@@ -10,9 +10,25 @@ def getTagInfo(filename):
     """
     return util.getTaggedWords(filename)
 
+def getStartingProb(tag_info):
+    starting_prob = {}
+    for sentence in tag_info:
+        startingTagInfo = sentence[0]
+
+        tag = startingTagInfo['tag']
+
+        if tag in starting_prob:
+            starting_prob[tag] += 1
+        else:
+            starting_prob[tag] = 1
+    return starting_prob
+
 if __name__ == '__main__':
     start = datetime.datetime.now()
+
     tag_info = getTagInfo(constant.DEV_TAGGED_DATA)
+    starting_prob = getStartingProb(tag_info)
+
     end = datetime.datetime.now()
     print 'Took ' + str(end-start) + ' time.'
 
