b758ee79fdab62acc02574b384438e2690bbbb00
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-05-03 22:31:52 -0700

    Adding comments.

--- Homework 4/calculatebleu.py
+++ Homework 4/calculatebleu.py
@@ -2,6 +2,10 @@ import sys
 import ngrams
 
 class ComputeBLEU(object):
+    '''
+    This class would compute the BLEU score. To initiate the class, you need to pass
+    two arguments /path/to/candidate /path/to/reference to read the candidate and reference files
+    '''
     def __init__(self):
         if len(sys.argv) < 3:
             sys.exit('Usage: %s /path/to/candidate /path/to/reference' % sys.argv[0])
@@ -9,18 +13,29 @@ class ComputeBLEU(object):
         self.reference_file = sys.argv[2]
 
     def getCandidateNGrams(self):
+        '''
+        Computation of the N-grams of the candidate file
+        '''
         ngram = ngrams.NGrams(self.candidata_file).getNGrams()
 
         self.candidate_ngrams = ngram
 
     def getReferenceNGrams(self):
+        '''
+        Computation of the N-grams of the reference file
+        '''
         ngram = ngrams.NGrams(self.reference_file).getNGrams()
 
         self.reference_ngrams = ngram
 
     def computeNgrams(self):
+        '''
+        Computation of the N-grams of the both the reference and candidate files
+        '''
+
         self.getCandidateNGrams()
         self.getReferenceNGrams()
+
 
 if __name__ == '__main__':
     blue = ComputeBLEU()
