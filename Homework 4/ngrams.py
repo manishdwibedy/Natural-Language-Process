f453d39b9776e70a09f39088ae1c2f590ade8215
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-05-03 21:41:45 -0700

    Adding comments.

--- Homework 4/ngrams.py
+++ Homework 4/ngrams.py
@@ -5,17 +5,24 @@ class NGrams(object):
         self.filename = filename
 
     def readFile(self):
+        '''
+        Reading the file
+        :return: Sets the value in the line parameter
+        '''
         lines = []
 
         with codecs.open(self.filename,'r',encoding='utf8') as f:
             self.lines = f.readlines()
-            pass
 
-    def computeNGrams(self):
+    def computeNGramsFile(self):
+        # Reading the file
         self.readFile()
+
+        # For each of the lines
         for line in self.lines:
+            # computing the list of words
             print line
 
 if __name__ == '__main__':
     file_location = 'data/candidate-1.txt'
-    NGrams(file_location).computeNGrams()
+    NGrams(file_location).computeNGramsFile()
