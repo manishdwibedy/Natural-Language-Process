7dec1678d6f779f5cdbed020a9b78553be20e0a8
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-05-03 23:21:14 -0700

    First made the n-gram calculation configurable. Next, all the computation has been modified for the new DS returned by the 'computeNgrams' logic.
    Also created a new method to get the number of words from the DS of ngrams.

--- Homework 4/calculatebleu.py
+++ Homework 4/calculatebleu.py
@@ -6,7 +6,8 @@ class ComputeBLEU(object):
     This class would compute the BLEU score. To initiate the class, you need to pass
     two arguments /path/to/candidate /path/to/reference to read the candidate and reference files
     '''
-    def __init__(self):
+    def __init__(self, n):
+        self.n = n
         if len(sys.argv) < 3:
             sys.exit('Usage: %s /path/to/candidate /path/to/reference' % sys.argv[0])
         self.candidata_file = sys.argv[1]
@@ -16,7 +17,7 @@ class ComputeBLEU(object):
         '''
         Computation of the N-grams of the candidate file
         '''
-        ngram = ngrams.NGrams(self.candidata_file).getNGrams()
+        ngram = ngrams.NGrams(self.n, self.candidata_file).getNGrams()
 
         self.candidate_ngrams = ngram
 
@@ -24,7 +25,7 @@ class ComputeBLEU(object):
         '''
         Computation of the N-grams of the reference file
         '''
-        ngram = ngrams.NGrams(self.reference_file).getNGrams()
+        ngram = ngrams.NGrams(self.n, self.reference_file).getNGrams()
 
         self.reference_ngrams = ngram
 
@@ -43,16 +44,30 @@ class ComputeBLEU(object):
         '''
         self.computeNgrams()
 
-        BLEU_score = 0
-        for candidate_ngram, candidate_count in self.candidate_ngrams.iteritems():
+        BLEU_Score = 0
+        for lineNumber, line in enumerate(self.candidate_ngrams):
+            line_BLEU_score = 0
+            for candidate_ngram, candidate_count in line.iteritems():
+                reference_line = self.reference_ngrams[lineNumber]
+                # the ngram is present in the reference line, as well
+                if candidate_ngram in reference_line:
+                    count = min(candidate_count, self.reference_ngrams[lineNumber][candidate_ngram])
+                    num_of_words = self.getWords(line)
+                    line_BLEU_score += float(count) / num_of_words
+            print 'Line - ' + str(lineNumber)
+            print line_BLEU_score
+            # print line_BLEU_score
+            BLEU_Score += line_BLEU_score
+        pass
+        # print BLEU_score / len(self.candidate_ngrams)
 
-            # the ngam is present in the reference file, as well
-            if candidate_ngram in self.reference_ngrams:
-                count = min(candidate_count, self.reference_ngrams[candidate_ngram])
-                BLEU_score += float(count) / len(candidate_ngram)
-        print BLEU_score
-
+    def getWords(self, ngram):
+        word_count = 0
+        for ngram_token, count in ngram.iteritems():
+            word_count += count
+        return word_count
 if __name__ == '__main__':
-    blue = ComputeBLEU()
+    n = 1
+    blue = ComputeBLEU(n)
     blue.computeBLUE()
     pass
