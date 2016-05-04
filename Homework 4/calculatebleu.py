3d5c78d601ec7b59ded9e91d7fa4d58786e262c4
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-05-03 22:27:46 -0700

    Now considering the reference file as well.

--- Homework 4/calculatebleu.py
+++ Homework 4/calculatebleu.py
@@ -3,16 +3,26 @@ import ngrams
 
 class ComputeBLEU(object):
     def __init__(self):
-        if len(sys.argv) < 2:
-            sys.exit('Usage: %s /path/to/candidate_file' % sys.argv[0])
+        if len(sys.argv) < 3:
+            sys.exit('Usage: %s /path/to/candidate /path/to/reference' % sys.argv[0])
         self.candidata_file = sys.argv[1]
+        self.reference_file = sys.argv[2]
 
     def getCandidateNGrams(self):
         ngram = ngrams.NGrams(self.candidata_file).getNGrams()
 
         self.candidate_ngrams = ngram
 
+    def getReferenceNGrams(self):
+        ngram = ngrams.NGrams(self.reference_file).getNGrams()
+
+        self.reference_ngrams = ngram
+
+    def computeNgrams(self):
+        self.getCandidateNGrams()
+        self.getReferenceNGrams()
 
 if __name__ == '__main__':
     blue = ComputeBLEU()
+    blue.computeNgrams()
     pass
