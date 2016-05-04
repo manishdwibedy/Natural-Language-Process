aea7e864cd30da00ace7cae5165ee1e0cc2d9ee3
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-05-04 12:59:32 -0700

    Now, using unigrams, bigrams, trigrams and quadgrams to compute the BLUEscore.
    Still need to incorporate the BLUE penalties to get the right score.

--- Homework 4/calculatebleu.py
+++ Homework 4/calculatebleu.py
@@ -1,72 +1,79 @@
 import sys
 import ngrams
+import math
 
 class ComputeBLEU(object):
     '''
     This class would compute the BLEU score. To initiate the class, you need to pass
     two arguments /path/to/candidate /path/to/reference to read the candidate and reference files
     '''
-    def __init__(self, n):
-        self.n = n
+    def __init__(self):
         if len(sys.argv) < 3:
             sys.exit('Usage: %s /path/to/candidate /path/to/reference' % sys.argv[0])
         self.candidata_file = sys.argv[1]
         self.reference_file = sys.argv[2]
 
-    def getCandidateNGrams(self):
+    def getCandidateNGrams(self, n):
         '''
         Computation of the N-grams of the candidate file
         '''
-        ngram = ngrams.NGrams(self.n, self.candidata_file).getNGrams()
+        ngram = ngrams.NGrams(n, self.candidata_file).getNGrams()
 
         self.candidate_ngrams = ngram
 
-    def getReferenceNGrams(self):
+    def getReferenceNGrams(self, n):
         '''
         Computation of the N-grams of the reference file
         '''
-        ngram = ngrams.NGrams(self.n, self.reference_file).getNGrams()
+        ngram = ngrams.NGrams(n, self.reference_file).getNGrams()
 
         self.reference_ngrams = ngram
 
-    def computeNgrams(self):
+    def computeNgrams(self, n):
         '''
         Computation of the N-grams of the both the reference and candidate files
         '''
 
-        self.getCandidateNGrams()
-        self.getReferenceNGrams()
+        self.getCandidateNGrams(n)
+        self.getReferenceNGrams(n)
 
     def computeBLUE(self):
         '''
 
         Computation of the BLEU score
         '''
-        self.computeNgrams()
+
+        result = []
+        for n in range(1,5):
+            self.computeNgrams(n)
+
+            BLEU_Score = 0
+            for lineNumber, line in enumerate(self.candidate_ngrams):
+                line_BLEU_score = 0
+                for candidate_ngram, candidate_count in line.iteritems():
+                    reference_line = self.reference_ngrams[lineNumber]
+                    # the ngram is present in the reference line, as well
+                    if candidate_ngram in reference_line:
+                        count = min(candidate_count, self.reference_ngrams[lineNumber][candidate_ngram])
+                        line_BLEU_score += float(count)
+                BLEU_Score += line_BLEU_score
+            part_result = BLEU_Score / self.getWords(self.candidate_ngrams)
+            result.append(part_result)
 
         BLEU_Score = 0
-        for lineNumber, line in enumerate(self.candidate_ngrams):
-            line_BLEU_score = 0
-            for candidate_ngram, candidate_count in line.iteritems():
-                reference_line = self.reference_ngrams[lineNumber]
-                # the ngram is present in the reference line, as well
-                if candidate_ngram in reference_line:
-                    count = min(candidate_count, self.reference_ngrams[lineNumber][candidate_ngram])
-                    num_of_words = self.getWords(line)
-                    line_BLEU_score += float(count) / num_of_words
-            print 'Line - ' + str(lineNumber)
-            print line_BLEU_score
-            # print line_BLEU_score
-            BLEU_Score += line_BLEU_score
-        print BLEU_Score / len(self.candidate_ngrams)
+        for r in result:
+            BLEU_Score += math.log(r+1)
+
+        BLEU_Score /= len(result)
+        print BLEU_Score
 
     def getWords(self, ngram):
         word_count = 0
-        for ngram_token, count in ngram.iteritems():
-            word_count += count
+        for ngam_line in ngram:
+            for ngram_token, count in ngam_line.iteritems():
+                word_count += count
         return word_count
 if __name__ == '__main__':
-    n = 2
-    blue = ComputeBLEU(n)
+    blue = ComputeBLEU()
     blue.computeBLUE()
     pass
